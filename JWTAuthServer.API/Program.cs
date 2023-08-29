f28c254b6f1350d066ab40c8326a95bbb9017df5
1 <eray.bydr@hotmail.com>
2023-08-29 12:06:26 +0300

    Connection string config for PostgreSQL

--- JWTAuthServer.API/Program.cs
+++ JWTAuthServer.API/Program.cs
@@ -1,4 +1,13 @@
 using JWTAuthServer.Core.Configuration;
+using JWTAuthServer.Core.Model;
+using JWTAuthServer.Core.Repositories;
+using JWTAuthServer.Core.Services;
+using JWTAuthServer.Core.UnitOfWork;
+using JWTAuthServer.Data;
+using JWTAuthServer.Data.Repositories;
+using JWTAuthServer.Service.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +16,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+//DI Register
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"), sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly("JWTAuthServer.Data");
+    });
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+    opt.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
